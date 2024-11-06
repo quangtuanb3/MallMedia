b75718ecaa9b311871b777e8e4bc7b1c404d9d53
nhathuynguyenhai1999 <nhathuynguyenhai@gmail.com>
2024-11-06 16:48:25 +0700

    fix code

--- MallMedia.Domain/Repositories/IScheduleRepository.cs
+++ MallMedia.Domain/Repositories/IScheduleRepository.cs
@@ -5,4 +5,5 @@ namespace MallMedia.Domain.Repositories;
 public interface IScheduleRepository
 {
     public Task<List<Device>> GetMatchingDevices(DateOnly StartDate, DateOnly EndDate, int ContentId, int TimeFrameId);
+    Task<Schedule> GetCurrentScheduleForDevice(object deviceId, object currentTime);
 }
